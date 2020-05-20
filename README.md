7ec8c6e17b01ab619d6e81a482af940afdc877b0
Fernando <dofernando@gmail.com>
2020-05-20 01:39:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## hendricss
+
+My simple boilerplate with Nunjucks and Stylus.
+
+### Status
+[![Top Language](https://img.shields.io/github/languages/top/frrrnd/hendricss?color=blueviolet&label=css%2Fstylus)](https://frrrnd.design)
+[![Code Size](https://img.shields.io/github/languages/code-size/frrrnd/hendricss?color=ff69b4)](https://github.com/frrrnd/blog)
+
+### Installation
+```
+git clone
+```
+
+#### Install gulp and npm deps
+``` 
+npm install
+```
+
+#### Browser sync and watch files
+```
+gulp
+```
+
+
+---
+
+### Licença
+
+Os diretórios ```/content```, ```/static/uploads``` e seus conteúdos e subdiretórios são registrados por mim, Fernando Soares, e não podem ser reproduzidos sem minha permissão.
+
+Tudo o resto é publicado sob a licença MIT, o que significa que você pode usá-lo livremente e como quiser. Eu ficaria feliz com um link para o site frrrnd.design, mas não é obrigatório.
+
+Copyright (c) 2019 Fernando Soares
---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const { src, dest, watch, series, parallel } = require('gulp');
+const stylus = require('gulp-stylus');
+const render = require('gulp-nunjucks-render');
+const browserSync = require('browser-sync').create();
+
+let paths = {
+    styles: {
+        src: "src/stylus/index.styl",
+        dest: "static/css"
+    },
+
+    html: {
+        src: "src/**/*.nj",
+        dest: "./"
+    },
+
+    js: {
+        src: "***/**/*.js",
+        dest: "static/js"
+    },
+
+    imgs: {
+        src: "src/**/*.jpg",
+        dest: "static/img"
+    },
+
+    fonts: {
+        src: "src/fonts",
+        dest: "static/fonts"
+    },
+
+    public: {
+        url: "./"
+    }
+}
+
+function sync(done) {
+    browserSync.init();
+    done();
+}
+
+function moveFonts() {
+    return (
+        src(paths.fonts.src)
+        .pipe(dest(paths.fonts.dest))
+        .pipe(browserSync.stream())
+    )
+}
+
+function stylusCompile() {
+    return (
+        src(paths.styles.src)
+        .pipe(stylus({
+            compress: true
+        }))
+        .pipe(dest(paths.styles.dest))
+        .pipe(browserSync.stream())
+    )
+}
+
+function nunjucksCompile() {
+    return (
+        src(paths.html.src)
+        .pipe(render({
+            path: ['src/partials']
+        }))
+        .pipe(dest(paths.public.url))
+        .pipe(browserSync.stream())
+    )
+}
+
+function watchFonts() {
+    watch(paths.fonts.src, moveFonts)
+}
+
+function watchHtml() {
+    watch(paths.html.src, nunjucksCompile)
+}
+
+function watchstylus() {
+    watch(paths.styles.src, stylusCompile)
+}
+
+exports.default = parallel(sync, watchstylus, watchHtml, watchFonts);
